d9f417eb1fab35893c19a7e052f3b35a92ea6fd5
Rajendra-Katuwal <raj.katuwal2061@gmail.com>
2025-11-11 12:16:39 +0545

    feat: completed workshop 3 task5

---
+++ Task5/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Task5;
+
+class Program
+{
+    static void PrintSumOfNumbers()
+    {
+        Console.WriteLine("Enter a number to find the sumation upto that number: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int sumation = 0;
+        
+        for (int i = 1; i <= n; i++)
+        {
+            sumation += i;
+        }
+        
+        Console.WriteLine($"Sum of numbers upto {n} is {sumation}.");
+    }
+
+    static void PrintNumbersUpto()
+    {
+        Console.WriteLine("Printing numbers between 1 and 20 inclusive: ");
+        int n = 1;
+
+        while (n <= 20)
+        {
+            Console.WriteLine(n);
+            n++;
+        }
+    }
+
+    static int PrintSumationArray(int[] arr)
+    {
+        int summation = 0;
+        foreach (var n in arr)
+        {
+            summation += n;
+        }
+
+        return summation;
+    }
+    
+    static void Main(string[] args)
+    {
+        PrintSumOfNumbers();
+        PrintNumbersUpto();
+        
+        int[] numbers = {2,5,6,8,10,22,66};
+        PrintSumationArray(numbers);
+    }
+}
