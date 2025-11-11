0702f680280529ff3a23921d04dae0e4dae00f87
Rajendra-Katuwal <raj.katuwal2061@gmail.com>
2025-11-11 11:55:49 +0545

    feat: completed workshop 3 task3

---
+++ Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string ageGroup;
+        Console.Write("Enter your Age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        if (age < 13)
+        {
+            ageGroup = "Child";
+        }
+        else if (age >= 13 && age <= 19)
+        {
+            ageGroup = "Teenager";
+        }
+        else if (age >= 20)
+        {
+            ageGroup = "Adult";
+        }
+        else
+        {
+            ageGroup = "Invalid Age Group";
+        }
+        
+        Console.WriteLine($"You are in {ageGroup}.");
+    }
+}
