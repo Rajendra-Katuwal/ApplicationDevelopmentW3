582b1dee821a1e09325d6c81cad0904591b19cf8
Rajendra-Katuwal <raj.katuwal2061@gmail.com>
2025-11-11 11:27:19 +0545

    feat: completed the workshop 3 Task1

---
+++ Task1/Operators.cs
@@ -0,0 +1,40 @@
+﻿namespace Task1;
+
+public class Operators
+{
+    // Takes two integer parameters and prints out their sumation.
+    public void Add(int a, int b)
+    {
+        int su = a + b;
+        Console.WriteLine($"{a} + {b} = {su}");
+    }
+
+    // Takes two int parameters and prints out the subtraction of b from a.
+    public void Subtract(int a, int b)
+    {
+        int sub = a - b;
+        Console.WriteLine($"{a} - {b} = {sub}");
+    }
+
+    // Takes two integer parameters and prints out their multiplication
+    public void Multiply(int a, int b)
+    {
+        int mul = a * b;
+        Console.WriteLine($"{a} * {b} = {mul}");
+    }
+
+    // Takes two integer parameters and prints out the division of a from b;
+    public void Divide(int a, int b)
+    {
+        int div = a / b;
+        Console.WriteLine($"{a} / {b} = {div}");
+    }
+    
+    // Takes an integer and prints if the number is Odd or Even
+    public void OddEvenFinder(int n)
+    {
+        string s = n % 2 == 0 ? "Even Number" : "Odd Number";
+        Console.WriteLine(s);
+    }
+
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task1;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Operators op = new Operators();
+        op.Add(1, 2);
+        op.Subtract(50, 22);
+        op.Multiply(10, 20);
+        op.Divide(10, 20);
+        
+        op.OddEvenFinder(21);
+    }
+}
