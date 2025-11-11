edf6bf51887711105b67003732e32966ed3bdfb7
Rajendra-Katuwal <raj.katuwal2061@gmail.com>
2025-11-11 12:26:17 +0545

    feat: completed workshop 3 Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Task6;
+
+class Program
+{
+    public static void ConvertToInteger()
+    {
+        string nstr = Console.ReadLine();
+        int n;
+
+        try
+        {
+            n = Convert.ToInt32(nstr);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Invalid Number Format");
+        }
+        finally
+        {
+            Console.WriteLine("Program Executed");
+        }
+    }
+
+
+    static void Password()
+    {
+        Console.Write("Enter a Password: ");
+        string nstr = Console.ReadLine();
+
+        if (nstr.Length < 6)
+        {
+            throw new Exception("Password must be at least 6 characters.");
+        }
+        else
+        {
+            Console.Write("Password requirement satisfied.");
+        }
+    }
+    
+    static void Main(string[] args)
+    {
+        ConvertToInteger();
+     
+        Password();
+    }
+}
