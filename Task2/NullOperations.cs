04787bfde90ad35e76e367a36b0b75a55850e9d8
Rajendra-Katuwal <raj.katuwal2061@gmail.com>
2025-11-11 11:41:26 +0545

    feat: completed workshop 3 Task2

---
+++ Task2/NullOperations.cs
@@ -0,0 +1,21 @@
+﻿namespace Task2;
+
+public class NullOperations
+{
+    public void PerformNullChecks()
+    {
+        string? username = null;
+
+        // null check using ternary operator
+        string checkResult = username == null ? "Username is not available" : username;
+        Console.WriteLine($"Ternary Operator: {checkResult}");
+        
+        // null check using null coalescing operator
+        string coalescingResult = username ?? "Username is not available";
+        Console.WriteLine($"Null Coalescing Assingment Operator: {coalescingResult}");
+        
+        // null coalescing assignment operator
+        username ??= "Default Username";
+        Console.WriteLine($"Null Coalescing Assingment Operator: {username}");
+    }
+}
